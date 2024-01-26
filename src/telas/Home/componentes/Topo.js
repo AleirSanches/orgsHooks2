7fe1e2a5efca3b5bd11c30757cf96adb89ce0e5a
Aleir Sanches <aleir.sanches@hotmail.com>
2024-01-25 23:44:59 -0300

    adicionando componentes no topo

--- src/telas/Home/componentes/Topo.js
+++ src/telas/Home/componentes/Topo.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
+import logo from '../../../assets/logo.png';
 
 export default function Topo(){
-    return <Text>   Que merdaaaaaaa </Text>
+    return <View style={estilos.topo}>
+            <Image style={estilos.image} source ={logo}/>
+            <Text style={estilos.boasVindas}> Ola Aleir </Text>
+            <Text style={estilos.legenda}> Encontre os melhores produtores</Text>
+        </View>
 }
+
+const estilos= StyleSheet.create({
+    topo: {
+        backgroundColor: '#F6F6F6',
+        padding: 16,
+    },
+    image: {
+        width:70,
+        height:28,
+    },
+    boasVindas:{
+        marginTop:22,
+        fontSize:26,
+        lineHeight:42,
+        fontWeight: 'bold',
+
+    },
+    legenda:{
+        fontSize:16,
+        lineHeight:26,
+
+    }
+
+});
